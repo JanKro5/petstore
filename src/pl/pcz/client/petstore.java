67efd6f38ed6b56f7b12b3396aa784ebea48a162
Student <student@debian>
2017-04-24 11:26:27 +0200

    events

--- src/pl/pcz/client/petstore.java
+++ src/pl/pcz/client/petstore.java
@@ -7,8 +7,14 @@ import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.HTMLTable.Cell;
 import com.google.gwt.user.client.ui.VerticalPanel;
-
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
+import com.google.gwt.core.client.Scheduler.RepeatingCommand;
 public class petstore implements EntryPoint {
 
     private FlexTable table = new FlexTable();
@@ -18,19 +24,54 @@ public class petstore implements EntryPoint {
 
     public void onModuleLoad() {
 	// Sample pet data.
+	final Label clickInfo = new HTML("Table Click: \n");
 	table.setText(0, 0, "123");
 	table.setText(0, 1, "Spot");
+	table.addClickHandler(new ClickHandler() {
+		int x =0;
+	   	public void onClick(ClickEvent event) {
+			Cell cell = table.getCellForEvent(event);
+			int receiverRowIndex = cell.getRowIndex(); // <- here!
+			++x;
+			table.setText(x,0,receiverRowIndex+"");
+			table.setText(x,1,cell.getElement().getInnerHTML());
+	
+    		}
+	});
+
+	Scheduler.get().scheduleFixedPeriod(new RepeatingCommand() {
+		int s=0;
+		int m=0;
+		int h =0;
+	        @Override
+	    	public boolean execute() {
+			++s;
+			if(s>60){
+				m++;
+				s=0;
+			}
+			if(m>60){
+				m=0;
+				h++;
+			}
+			if(h>24){
+				h=0;
+			}
+			clickInfo.setText(h+":"+m+":"+s+"");
+			return true;
+	        }
+	},1000);
 
 	VerticalPanel right = new VerticalPanel();
 	FlowPanel f1 = new FlowPanel();
 	f1.add(new Label("ID:"));
 	f1.add(id);
 	FlowPanel f2 = new FlowPanel();
-	f1.add(new Label("Name:"));
-	f1.add(name);
+	f2.add(new Label("Name:"));
+	f2.add(name);
 	FlowPanel f3 = new FlowPanel();
-	f1.add(new Label("Category:"));
-	f1.add(category);
+	f3.add(new Label("Category:"));
+	f3.add(category);
 	right.add(f1);
 	right.add(f2);
 	right.add(f3);
@@ -38,6 +79,7 @@ public class petstore implements EntryPoint {
 	HorizontalPanel middle = new HorizontalPanel();
 	middle.add(table);
 	middle.add(right);
+	middle.add(clickInfo);
 	middle.addStyleName("middle");
 	Label header = new Label("Petstore");
 	header.addStyleName("header");
